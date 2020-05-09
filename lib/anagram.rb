17945c1cb254dacfa2ee7ead72181fa2bfb7a7e5
Betelhem Elfagid <betty_elfagid@yahoo.co.uk>
2020-05-09 23:07:00 +0000

    Automatically backed up by Learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -1,1 +1,15 @@
 # Your code goes here!
+require 'pry'
+class Anagram
+  attr_accessor :word
+  
+  def intialize(word)
+    @word = word 
+  end 
+  
+  def match(array_of_words)
+    binding.pry
+  end 
+end 
+   
+    
